477369072bf40ba5cad2f835e3541c6f005c762a
Sheikhman_Kalderon <duah229@outlook.com>
2025-06-30 16:45:09 +0000

    feature:add product creation request to the project

---
+++ product-service/src/main/java/com/ecommerce/productservice/dto/ProductRequestDto.java
@@ -0,0 +1,17 @@
+package com.ecommerce.productservice.dto;
+
+import lombok.*;
+/*
+I can omit setters here if I am planning immutability
+ */
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ProductRequestDto {
+    private String productName;
+    private Long productPrice;
+    private Long productQuantity;
+}
