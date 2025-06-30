705d0b387d3226330c5cb4a02122bd8dc1c7bfd3
Sheikhman_Kalderon <duah229@outlook.com>
2025-06-30 18:48:33 +0000

    feature: add custom methods in repository layer

--- product-service/src/main/java/com/ecommerce/productservice/repository/ProductRepository.java
+++ product-service/src/main/java/com/ecommerce/productservice/repository/ProductRepository.java
@@ -3,5 +3,13 @@ package com.ecommerce.productservice.repository;
 import com.ecommerce.productservice.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    /*
+    spring data would then allow you to use findById for free, but I defined as productId
+    so I am defining it here to get sql generated for me
+     */
+    Optional<Product> findByProductId(Long productId);
+    void deleteByProductId(Long productId);
 }
