f48e86ad4bdf4e5f9aeb390445ba997bde6e82cb
Sheikhman_Kalderon <duah229@outlook.com>
2025-06-30 16:18:50 +0000

    feature: add project structure and product entity

---
+++ product-service/src/main/java/com/ecommerce/productservice/entity/Product.java
@@ -0,0 +1,29 @@
+package com.ecommerce.productservice.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+@Entity
+@Table(name = "product")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long productId;
+
+    @Column(nullable = false)
+    private String productName;
+
+    @Column(nullable = false)
+    private Long productPrice;
+
+    @Column(nullable = false)
+    private Long productQuantity;
+}
