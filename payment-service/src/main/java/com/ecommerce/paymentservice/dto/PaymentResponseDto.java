66e9716763506fb7ab597c68d5b6fee398a5edb7
Sheikhman_Kalderon <duah229@outlook.com>
2025-07-01 22:59:36 +0000

    feature: add payment response dto the project

---
+++ payment-service/src/main/java/com/ecommerce/paymentservice/dto/PaymentResponseDto.java
@@ -0,0 +1,22 @@
+package com.ecommerce.paymentservice.dto;
+
+import com.ecommerce.paymentservice.entity.PaymentMode;
+import lombok.*;
+
+import java.time.Instant;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class PaymentResponseDto {
+    private Long paymentId;
+    private String paymentStatus;
+    private PaymentMode paymentMode;
+    private Double amount;
+    private Instant paymentDate;
+    private Long orderId;
+
+}
