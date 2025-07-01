142947059a81a4f6e8815d9f7a2f9ce4764172fc
Sheikhman_Kalderon <duah229@outlook.com>
2025-07-01 22:20:40 +0000

    feature: add entity to the project for mongo db

---
+++ order-service/src/main/java/com/ecommerce/orderservice/entity/TransactionDetails.java
@@ -0,0 +1,34 @@
+package com.ecommerce.orderservice.entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.Instant;
+/*
+usage of mongodb to create entities
+very simple and effective
+ */
+
+@Document(collection="transactions_details")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class TransactionDetails {
+
+    @Id
+    private String id; //mongo auto generates Object id stored as a string
+    private Long orderId;
+    private String paymentMode;
+    private String referenceNumber;
+    private Instant paymentDate;
+    private String paymentStatus;
+    private Double amount;
+
+
+}
