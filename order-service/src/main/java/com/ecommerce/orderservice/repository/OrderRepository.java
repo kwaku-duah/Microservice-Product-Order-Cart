296b695515a2f8248aa413fc7d5ec32721df6334
Sheikhman_Kalderon <duah229@outlook.com>
2025-07-01 22:27:01 +0000

    feature: add custom method to find a transaction detail with order id

--- order-service/src/main/java/com/ecommerce/orderservice/repository/OrderRepository.java
+++ order-service/src/main/java/com/ecommerce/orderservice/repository/OrderRepository.java
@@ -3,5 +3,9 @@ package com.ecommerce.orderservice.repository;
 import com.ecommerce.orderservice.entity.TransactionDetails;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.Optional;
+
 public interface OrderRepository extends MongoRepository<TransactionDetails, String> {
+    Optional<TransactionDetails> findByOrderId(Long orderId);
 }
+
